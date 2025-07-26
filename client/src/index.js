9dd9ca0bb824d5fb71c811d982da65eccd7c5adb
Shahzad Hussain <hittheyellow1@gmail.com>
2025-07-26 20:58:51 +0530

    Initial commit with React frontend

---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
+import "./styles.css"; // ✅ updated path after moving styles.css into src/
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
