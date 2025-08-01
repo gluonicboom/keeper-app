d153be5b445035bfe964f8d646e9b16a7416a55a
Shahzad Hussain <hittheyellow1@gmail.com>
2025-08-01 18:17:25 +0530

    feat(ui): Replace header icon with custom brain symbol

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import HighlightIcon from "@mui/icons-material/Highlight";
-
+import BrainIcon from "./BrainIcon";
 function Header() {
   return (
     <>
       <header className='bg-gray-500  px-8 py-4 shadow-md'>
         <h1 className= 'text-[#fff] font-serif font-extralightd text-2xl '>
-          <HighlightIcon />
-          Brain Extension
+         <BrainIcon fontSize = "large"/>
+        {""} Brain Extension
         </h1>
       </header> 
     </>
